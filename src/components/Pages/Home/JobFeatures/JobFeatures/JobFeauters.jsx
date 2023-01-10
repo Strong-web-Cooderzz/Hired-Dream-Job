8d6268699dc590f8b59019abf2ff64fec0649285
Md.Ashrafull Islam <ashrafull130@gmail.com>
2023-01-11 01:27:20 +0600

    added update test pull

--- src/components/Pages/Home/JobFeatures/JobFeatures/JobFeauters.jsx
+++ src/components/Pages/Home/JobFeatures/JobFeatures/JobFeauters.jsx
@@ -4,7 +4,8 @@ const JobFeauters = () => {
     return (
         <div>
             <h2> Here is Job Features section </h2>
-            
+            <h2> Job Features List  </h2>
+
             
         </div>
     );
