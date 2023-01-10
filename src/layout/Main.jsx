fba9db3b5b694170a7c89489c0ac5a3253e09762
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2023-01-10 11:22:58 -0800

    update

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Home from '../components/Pages/Home/Home';
+import Testimonial from '../components/Pages/Testimonial/Testimonial';
 import Footer from '../components/shared/Footer/Footer';
 import Navbar from '../components/shared/Navbar/Navbar';
 
@@ -9,6 +10,7 @@ const Main = () => {
         <div>
           <Navbar />
             <Home />
+            <Testimonial/>
           <Footer />
         </div>
     );
