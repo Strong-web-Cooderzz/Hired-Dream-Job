deade1fd847e2b89402a18c7a60db4d63c0e1ee6
Arman Khan <sparmankhan@gmail.com>
2023-01-10 23:54:21 +0600

    tailwind react-router-dom installed

---
+++ src/components/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Hero from './Hero/Hero';
+
+const Home = () => {
+    return (
+        <div>
+            <Hero />
+            <h2>Home</h2>
+        </div>
+    );
+};
+
+export default Home;
