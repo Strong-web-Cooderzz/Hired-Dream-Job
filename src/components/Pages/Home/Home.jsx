89dc70c56319a0b5144d8b1810629ef8851daf2c
Abid Hasan <109639011+abhsn@users.noreply.github.com>
2023-01-11 16:11:02 +0600

    fixed home
    didn't merge properly first time

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
-<<<<<<< HEAD
-import Articles from './Articles/Articles';
-=======
 import FeaturedCities from './FeaturedCities/FeaturedCities';
->>>>>>> footer-abid
+import Articles from './Articles/Articles';
 import Hero from './Hero/Hero';
 
 const Home = () => {
     return (
         <div>
             <Hero />
-<<<<<<< HEAD
             <Articles></Articles>
-=======
             <FeaturedCities />
->>>>>>> footer-abid
         </div>
     );
 };
