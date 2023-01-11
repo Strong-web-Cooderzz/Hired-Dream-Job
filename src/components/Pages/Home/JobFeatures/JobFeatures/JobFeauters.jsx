afb7e583b3e891d8ef71d3f3306410e6e7a79a54
Md.Ashrafull Islam <ashrafull130@gmail.com>
2023-01-11 11:00:26 +0600

    added google-font-poppins

--- src/components/Pages/Home/JobFeatures/JobFeatures/JobFeauters.jsx
+++ src/components/Pages/Home/JobFeatures/JobFeatures/JobFeauters.jsx
@@ -5,6 +5,7 @@ const JobFeauters = () => {
         <div>
             <h2> Here is Job Features section </h2>
             <h2> Job Features List  </h2>
+            <h2> Its for testing and learning purpose .</h2>
 
             
         </div>
