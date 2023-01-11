6190433b0c3e6a1c28f28452ca8d9f1eca0e8e25
Razibul Islam <razibulislam665@gmail.com>
2023-01-11 17:19:04 +0600

    nav and category

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import FeaturedCities from './FeaturedCities/FeaturedCities';
-import Articles from './Articles/Articles';
-import Hero from './Hero/Hero';
+import React from "react";
+import FeaturedCities from "./FeaturedCities/FeaturedCities";
+import Articles from "./Articles/Articles";
+import Hero from "./Hero/Hero";
+import JobCategory from "./JobCategory/JobCategory";
 
 const Home = () => {
-    return (
-        <div>
-            <Hero />
-            <Articles></Articles>
-            <FeaturedCities />
-        </div>
-    );
+  return (
+    <div>
+      <Hero />
+      <JobCategory />
+      <Articles></Articles>
+      <FeaturedCities />
+    </div>
+  );
 };
 
 export default Home;
