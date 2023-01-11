defa28cc1aae4f47a48d00bc25a0d1a6861b0833
Md.Ashrafull Islam <ashrafull130@gmail.com>
2023-01-11 20:29:05 +0600

    job category widht fixed and title fixed

--- src/components/Pages/Home/Articles/Articles.jsx
+++ src/components/Pages/Home/Articles/Articles.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Articles = () => {
     return (
         <div>
-            <h1> Recent News Articles</h1>
+            <h1 className='text-3xl text-center my-5 font-semibold'> Recent News Articles</h1>
         </div>
     );
 };
