f6549abc215ed78ad39688c221592e283f4c9760
Abid Hasan <ab1dhasan@outlook.com>
2023-01-11 08:28:48 +0600

    added copyright text to footer

--- src/components/shared/Footer/Footer.jsx
+++ src/components/shared/Footer/Footer.jsx
@@ -1,7 +1,23 @@
+import { IoIosArrowUp } from 'react-icons/io';
+
 export default function Footer() {
+  const moveToTop = () => {
+    window.scroll({
+      top: 0,
+      behavior: 'smooth'
+    })
+  }
+
   return (
     <footer>
-      2023 &copy; Hired Dream Job
+      {/* social icons and copyright */}
+      <div>
+        <span className="text-sm">
+          &copy; 2023 Hired Dream Job. All Right Reserved.
+        </span>
+      </div>
+      {/* button to scroll to top */}
+      <span onClick={moveToTop} className="cursor-pointer font-black font-2xl text-blue-700 fixed bottom-6 right-6 bg-blue-200 p-3 rounded-full"><IoIosArrowUp /></span>
     </footer>
   );
 }
