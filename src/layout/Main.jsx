8f031688345916c510707e702bbda0bf670a2a66
Abid Hasan <ab1dhasan@outlook.com>
2023-01-11 08:37:58 +0600

    added height to full to move footer at the bottom

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -5,13 +5,15 @@ import Footer from '../components/shared/Footer/Footer';
 import Navbar from '../components/shared/Navbar/Navbar';
 
 const Main = () => {
-    return (
-        <div>
-          <Navbar />
-            <Home />
-          <Footer />
-        </div>
-    );
+  return (
+    <div className="flex flex-col h-screen">
+      <Navbar />
+      <div className="grow">
+      <Home />
+    </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Main;
