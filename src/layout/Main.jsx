11b221739521bfa24243075d16e49462ff407239
Razibul Islam <razibulislam665@gmail.com>
2023-01-12 13:51:25 +0600

    Login and Register file are created

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Home from '../components/Pages/Home/Home';
-import Testimonials from '../components/Pages/Testimonial/Testimonials';
 import Footer from '../components/shared/Footer/Footer';
 import Navbar from '../components/shared/Navbar/Navbar';
 
