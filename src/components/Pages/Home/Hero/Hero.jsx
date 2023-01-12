4508aad56b30d82a613d96898430a947e288fb45
Arman Ali Khan <74469015+arman-ali-khan@users.noreply.github.com>
2023-01-12 08:36:47 +0600

    Update Hero.jsx

--- src/components/Pages/Home/Hero/Hero.jsx
+++ src/components/Pages/Home/Hero/Hero.jsx
@@ -7,7 +7,7 @@ import Companies from "./Companies";
 const Hero = () => {
   return (
     <div className="md:pl-20 overflow-hidden lg:pl-24">
-      <div className="w-full sm:flex text-center sm:text-left  relative">
+      <div className="w-full sm:flex text-white sm:text-black text-center sm:text-left  relative">
         <div className="lg:w-7/12 mx-8 z-10 flex items-center justify-center pt-24 sm:pt-56">
           <div className="mb-2">
             {/* Title */}
