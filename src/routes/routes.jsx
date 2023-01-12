af3d01f07cdbff58e6493f92c9c410e5edbde077
Arman Khan <sparmankhan@gmail.com>
2023-01-12 08:16:27 +0600

    routes added

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,8 +1,11 @@
 
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../components/Pages/Home/Home";
 import Main from "../layout/Main";
 
 
 export const router = createBrowserRouter([
-    {path:'/',element:<Main />}
+    {path:'/',element:<Main />, children:[
+        {path:'/', element:<Home />}
+    ]}
 ])
