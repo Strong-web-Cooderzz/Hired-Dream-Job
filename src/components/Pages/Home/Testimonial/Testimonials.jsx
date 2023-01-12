364cfd02936b543e0d96d937f5dae5186a9fa928
Razibul Islam <razibulislam665@gmail.com>
2023-01-12 20:48:50 +0600

    Login page design

--- src/components/Pages/Home/Testimonial/Testimonials.jsx
+++ src/components/Pages/Home/Testimonial/Testimonials.jsx
@@ -7,7 +7,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 
 // import required modules
-import { Navigation, Mousewheel, Keyboard } from "swiper";
+import { Keyboard, Mousewheel, Navigation } from "swiper";
 import Testimonial from "./Testimonial";
 
 const ArrOfTestimonials = [
@@ -75,12 +75,12 @@ const ArrOfTestimonials = [
 const Testimonials = () => {
   return (
     <div>
-      <section class="text-gray-600 body-font">
-        <div class="container px-5 py-24 mx-auto">
-          <h1 class="text-3xl font-medium title-font text-gray-900 mb-12 text-center">
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-24 mx-auto">
+          <h1 className="text-3xl font-medium title-font text-gray-900 mb-12 text-center">
             Testimonials
           </h1>
-          <div class="flex flex-wrap -m-4">
+          <div className="flex flex-wrap -m-4">
             <Swiper
               slidesPerView={2}
               spaceBetween={30}
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAjOeg1nZA50d6_ossoRWFKEE0LdMVjB-Y",
+  authDomain: "hired-dream-job.firebaseapp.com",
+  projectId: "hired-dream-job",
+  storageBucket: "hired-dream-job.appspot.com",
+  messagingSenderId: "538787129570",
+  appId: "1:538787129570:web:d43491f771a4adf95816bb",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
