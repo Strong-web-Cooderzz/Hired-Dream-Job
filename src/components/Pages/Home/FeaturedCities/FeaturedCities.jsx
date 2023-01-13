fe56e263dd4950241a2d60d5d9f977382bda85a8
Abid Hasan <ab1dhasan@outlook.com>
2023-01-13 17:15:53 +0600

    fixed featured cities

--- src/components/Pages/Home/FeaturedCities/FeaturedCities.jsx
+++ src/components/Pages/Home/FeaturedCities/FeaturedCities.jsx
@@ -14,7 +14,7 @@ export default function FeaturedCities() {
     const { name, jobs, image } = city;
     return (
       <div className="w-full  mx-auto">
-        <div className="flex justify-center   overflow-hidden rounded-lg relative">
+        <div className="flex justify-center overflow-hidden rounded-lg relative">
           <img src={image} alt={name} className="object-cover h-60 w-full rounded-lg hover:scale-110 transition-transform" />
           <p className="absolute text-white bottom-12 left-4 font-bold text-xl">{name}</p>
           <p className="absolute text-white bottom-4 left-4 text-sm">{jobs} {jobs > 0 ? 'Jobs' : 'Job'}</p>
@@ -27,7 +27,7 @@ export default function FeaturedCities() {
     <div className="w-full mx-auto px-5 ">
       <div className="my-6 text-center">
         <p className="font-bold text-3xl">Featured Cities</p>
-        <p>Get all of the job informations from our the top cities</p>
+        <p>Get all of the job informations from the top cities</p>
       </div>
       <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  place-content-center max-w-screen-2xl 2xl:mx-auto w-full gap-8">
         {
