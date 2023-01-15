b185da01c918141951a9ca459b0a228db32ae79b
Abid Hasan <ab1dh4san@gmail.com>
2023-01-15 20:46:25 +0600

    added sorting option

--- src/components/Pages/Employers/Employers.jsx
+++ src/components/Pages/Employers/Employers.jsx
@@ -6,6 +6,7 @@ import { IoIosArrowDown } from 'react-icons/io';
 export default function Employers() {
 	return (
 		<main className="w-full">
+			{/* search form starts here */}
 			<section className="bg-blue-50 w-full justify-center p-12 grid items-center gap-6">
 				{/* search form */}
 				<form className="flex gap-8 px-8 py-4 rounded-md bg-white">
@@ -51,6 +52,27 @@ export default function Employers() {
 						<option value="">Test</option>
 						<option value="">Test</option>
 					</select>
+				</div>
+			</section>
+
+			{/* search results */}
+			<section className="px-12 py-12">
+				<div className="flex justify-between items-center">
+					{/* shows how many jobs available */}
+					<span>
+						24 jobs
+					</span>
+
+					{/* sorting options */}
+					<div className="flex gap-4">
+						<select className="bg-gray-100 px-6 py-2" name="sort-by" id="sort-by">
+							<option value="">Sort by (default)</option>
+						</select>
+
+						<select className="bg-gray-100 px-6 py-2 rounded-md" name="pagination" id="pagination">
+							<option value="">All</option>
+						</select>
+					</div>
 				</div>
 			</section>
 		</main>
