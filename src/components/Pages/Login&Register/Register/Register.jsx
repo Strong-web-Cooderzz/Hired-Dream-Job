6f0d4332005cae935dfd4d99e7511e53cddcb272
Razibul Islam <razibulislam665@gmail.com>
2023-01-16 01:49:06 +0600

    Account Setting Added

--- src/components/Pages/Login&Register/Register/Register.jsx
+++ src/components/Pages/Login&Register/Register/Register.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { toast } from "react-hot-toast";
+import { Link, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../AuthProvider/AuthProvider";
 
 const Register = () => {
@@ -11,17 +12,59 @@ const Register = () => {
     formState: { errors },
     reset,
   } = useForm();
-  const { createAccount, user, loading } = useContext(AuthContext);
+
+  const imgbbAPIKEY = "baca7cebf7d1365bf97c10bb391342f9";
+  const navigate = useNavigate();
+
+  const { createAccount, user, loading, updateUserProfile } =
+    useContext(AuthContext);
+
   const onSubmit = (data) => {
-    console.log(data);
+    const image = data.image[0];
+    const formData = new FormData();
+    formData.append("image", image);
+
+    console.log(image, data);
+
+    const url = `https://api.imgbb.com/1/upload?key=${imgbbAPIKEY}`;
     const { email, password } = data;
-    createAccount(email, password)
-      .then((result) => {
-        console.log(result);
-        reset();
-      })
-      .catch((err) => {
-        console.error(err);
+    fetch(url, {
+      method: "POST",
+      body: formData,
+    })
+      .then((res) => res.json())
+      .then((imgbb) => {
+        console.log(imgbb.data.url);
+        createAccount(email, password)
+          .then((result) => {
+            console.log(result);
+
+            const displayName = data.firstName + data.lastName;
+            const photoURL = imgbb.data.url;
+
+            console.log(displayName, photoURL);
+
+            const info = {
+              displayName,
+              photoURL,
+            };
+            updateUserProfile(info)
+              .then((result) => {
+                console.log(result);
+                reset();
+                toast.success("Successfully Login");
+                if (data.type === "Client") {
+                  navigate("/accountClient");
+                } else {
+                  navigate("/accountAgency");
+                }
+              })
+              .catch((err) => console.error(err));
+          })
+
+          .catch((err) => {
+            console.error(err);
+          });
       });
   };
 
@@ -30,14 +73,21 @@ const Register = () => {
       <div className="container mx-auto">
         <div className="flex justify-center px-6 my-12">
           <div className="w-full xl:w-3/4 lg:w-11/12 flex">
-            <div className="w-full h-auto bg-gray-400 hidden lg:block lg:w-5/12 bg-cover rounded-l-lg bg-[url('https://img.freepik.com/free-vector/sign-concept-illustration_114360-125.jpg?w=740&t=st=1673543146~exp=1673543746~hmac=e420d0300850c37da6633d8c37aa9aa7d99132b9d171afddcf07600e0c43180d')]"></div>
-            <div className="w-full lg:w-7/12 bg-white p-5 rounded-lg lg:rounded-l-none">
+            <div className="w-full h-auto hidden lg:block lg:w-5/12 bg-cover rounded-l-lg ">
+              <div className="flex items-center h-full">
+                <img
+                  src="https://img.freepik.com/free-vector/sign-concept-illustration_114360-125.jpg?w=740&t=st=1673543146~exp=1673543746~hmac=e420d0300850c37da6633d8c37aa9aa7d99132b9d171afddcf07600e0c43180d"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="w-full lg:w-7/12 bg-white lg:p-5 rounded-lg lg:rounded-l-none">
               <h3 className="pt-4 text-2xl text-center font-semibold">
                 Create an Account!
               </h3>
               <form
                 onSubmit={handleSubmit(onSubmit)}
-                className="px-8 pt-6 pb-8 mb-4 bg-white rounded"
+                className="lg:px-8 lg:pt-6 lg:pb-8 lg:mb-4 bg-white rounded"
               >
                 <div className="mb-4 md:flex md:justify-between">
                   <div className="mb-4 md:mr-2 md:mb-0 w-full">
@@ -79,6 +129,7 @@ const Register = () => {
                     {errors.lastName && <span>{errors.lastName?.message}</span>}
                   </div>
                 </div>
+
                 <div className="mb-4">
                   <label
                     className="block mb-2 text-sm font-bold text-gray-700"
@@ -97,6 +148,58 @@ const Register = () => {
                   />
                   {errors.email && <span>{errors.email?.message}</span>}
                 </div>
+                <div className="mb-4">
+                  <label
+                    htmlFor="photo"
+                    className="block mb-2 text-sm font-bold text-gray-700"
+                  >
+                    Choose your Photo
+                  </label>
+                  <label>
+                    <input
+                      type="file"
+                      id="photo"
+                      accept="image/*"
+                      {...register("image", {
+                        required: "Photo Is Required",
+                      })}
+                      className="text-sm text-gray-700 file:mr-5 file:py-2 file:px-6 file:rounded-lg file:border-0 file:text-sm file:font-medium file:bg-blue-50 file:text-blue-500 hover:file:cursor-pointer hover:file:bg-amber-50 hover:file:text-amber-700 w-full bg-gray-50 rounded-lg border shadow"
+                    />
+                    {errors.image && <span>{errors.image?.message}</span>}
+                  </label>
+                </div>
+
+                <div className="mb-4">
+                  <div className="flex border rounded-lg overflow-hidden select-none w-full">
+                    <div className="py-3 my-auto px-5 bg-blue-500 text-white text-sm font-semibold mr-3">
+                      Account Type
+                    </div>
+                    <label className="flex radio p-2 cursor-pointer">
+                      <input
+                        className="my-auto transform scale-125"
+                        type="radio"
+                        {...register("type", {
+                          required: "Account Type Is Required",
+                        })}
+                        value="Agency"
+                      />
+                      <div className="px-2">Agency</div>
+                    </label>
+
+                    <label className="flex radio p-2 cursor-pointer">
+                      <input
+                        className="my-auto transform scale-125"
+                        type="radio"
+                        {...register("type", {
+                          required: "Account Type Is Required",
+                        })}
+                        value="Client"
+                      />
+                      <div className="title px-2">Client</div>
+                    </label>
+                  </div>
+                </div>
+
                 <div className="mb-4 md:flex md:justify-between">
                   <div className="mb-4 md:mr-2 md:mb-0 w-full">
                     <label
