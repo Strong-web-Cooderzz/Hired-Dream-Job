c8532960bf9d0ffaa8c2d1478ef8b7e323e0e9d6
Abid Hasan <ab1dh4san@gmail.com>
2023-01-16 04:12:33 +0600

    gets only specific employer data

--- src/components/Pages/Employer/Employer.jsx
+++ src/components/Pages/Employer/Employer.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 
 export default function Employer() {
 	const employerId = useParams().id;
+	const [employer, setEmployer] = useState([]);
+
+	useEffect(() => {
+		fetch('/data/employers.json')
+			.then(res => res.json())
+			.then(data => {
+				const newData = data.filter(i => employerId === i.id + '');
+				setEmployer(newData);
+			});
+	}, []);
+
 	return (
 		<main>
 
