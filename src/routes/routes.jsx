c8eeaaf6dc94be7aa1fa8a61d57bd07ad501055e
Abid Hasan <ab1dh4san@gmail.com>
2023-01-15 16:26:50 +0600

    added employers to routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
+import Employers from "../components/Pages/Employers/Employers";
 import Home from "../components/Pages/Home/Home";
 import Login from "../components/Pages/Login&Register/Login/Login";
 import ResetPass from "../components/Pages/Login&Register/Login/ResetPass";
@@ -7,27 +8,31 @@ import NotFound from "../components/Pages/NotFound/NotFound";
 import Main from "../layout/Main";
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    errorElement: <NotFound />,
-    element: <Main />,
-    children: [
-      {
+    {
         path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/register",
-        element: <Register />,
-      },
-    ],
-  },
-  {
-    path: "/resetPass",
-    element: <ResetPass />,
-  },
+        errorElement: <NotFound />,
+        element: <Main />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: '/employers',
+                element: <Employers />
+            },
+            {
+                path: "/login",
+                element: <Login />,
+            },
+            {
+                path: "/register",
+                element: <Register />,
+            },
+        ],
+    },
+    {
+        path: "/resetPass",
+        element: <ResetPass />,
+    },
 ]);
