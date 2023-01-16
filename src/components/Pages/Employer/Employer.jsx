2f9339d72459af8cfdf454612dc66f05d6ca9260
Abid Hasan <ab1dh4san@gmail.com>
2023-01-16 09:42:05 +0600

    added small details section

--- src/components/Pages/Employer/Employer.jsx
+++ src/components/Pages/Employer/Employer.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
 import { FaMapMarkerAlt } from "react-icons/fa";
 import { BsFillBagFill } from "react-icons/bs";
-import { BsTelephone } from "react-icons/bs";
+import { BsTelephone, BsLinkedin } from "react-icons/bs";
 import { HiOutlineMail } from "react-icons/hi";
 
 export default function Employer() {
@@ -46,6 +46,24 @@ export default function Employer() {
 			</section>
 
 			{/* details */}
+			<secction className="px-12 grid gap-16">
+				<div className="col-start-1 col-end-9">
+					<span>About Company</span>
+					<span>Lorem</span>
+				</div>
+
+				<div className="col-start-9 col-end-13">
+					<div className="flex flex-col bg-blue-50 p-6 gap-4">
+						<span className="flex items-center justify-between">Primary industry: <span className="text-gray-500 text-sm">Software</span></span>
+						<span className="flex items-center justify-between">Company Size: <span className="text-gray-500 text-sm">501-1000</span></span>
+						<span className="flex items-center justify-between">Founded in: <span className="text-gray-500 text-sm">2011</span></span>
+						<span className="flex items-center justify-between">Phone: <span className="text-gray-500 text-sm">123123123</span></span>
+						<span className="flex items-center justify-between">Location: <span className="text-gray-500 text-sm">{employer.location}</span></span>
+						<span className="flex items-center justify-between">Social media: <span className="text-gray-500 text-sm"><BsLinkedin /></span></span>
+						<button className="bg-blue-200 py-3 text-blue-700 rounded-md">www.site.com</button>
+					</div>
+				</div>
+			</secction>
 		</main>
 	)
 }
