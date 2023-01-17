6e23310c22cbf23351b9f20ee7ee582ddbe9ac1a
Abid Hasan <ab1dh4san@gmail.com>
2023-01-17 20:26:35 +0600

    added mobile responsiveness to card

--- src/components/Pages/Employers/Employers.jsx
+++ src/components/Pages/Employers/Employers.jsx
@@ -86,14 +86,14 @@ export default function Employers() {
 
 			{/* search results */}
 			<section className="px-12 py-12">
-				<div className="flex justify-between items-center">
+				<div className="grid grid-cols-2 gap-y-6 md:gap-y-0 md:flex justify-between items-center">
 					{/* shows how many jobs available */}
-					<span>
+					<span className="col-start-1 col-end-3 text-center">
 						{search.length} jobs
 					</span>
 
 					{/* sorting options */}
-					<div className="flex gap-4">
+					<div className="flex md:gap-4 col-start-1 col-end-3 justify-center gap-3">
 						<select className="bg-gray-100 px-6 py-2" name="sort-by" id="sort-by">
 							<option value="">Sort by (default)</option>
 						</select>
@@ -106,7 +106,7 @@ export default function Employers() {
 			</section>
 
 			{/* cards */}
-			<div className="px-12 grid grid-cols-4 gap-6">
+			<div className="px-6 md:px-12 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
 				{
 					search.length && search.map((i, id) => <SearchResultCard key={id} employer={i} />)
 				}
