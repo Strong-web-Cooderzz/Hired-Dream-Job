17d8807e9634da8dfc7826cfe52025fd6247029f
unknown <mojahidislammishat@gmail.com>
2023-01-19 18:45:57 +0000

    about single

---
+++ src/components/Pages/TermsPages/TremsPages.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const TremsPages = () => {
+    return (
+        <section>
+  <div>
+   <div className="">
+    <h2 className="text-center font-bold text-2xl mt-5">Terms and Conditions</h2>
+<Link to='/' className="text-center">Home / Terms and Conditions</Link>
+
+   </div>
+  
+    <div className="my-8 lg:w-1/2 md:w-full mx-auto">
+      <div className="my-5">
+        <h1 className="font-bold mb-4 text-2xl">1. Terms</h1> 
+<p className="mb-4">Pharetra nulla ullamcorper sit lectus. Fermentum mauris pellentesque nec nibh sed et, vel diam, massa. Placerat quis vel fames interdum urna lobortis sagittis sed pretium. Aliquam eget posuere sit enim elementum nulla vulputate magna. Morbi sed arcu proin quis tortor non risus</p>
+<p>Elementum lectus a porta commodo suspendisse arcu, aliquam lectus faucibus. Nisl malesuada tortor, ligula aliquet felis vitae enim. Mi augue aliquet mauris non elementum tincidunt eget facilisi. Pellentesque massa ipsum tempus vel aliquam massa eu pulvinar eget.</p>      
+</div>
+<div className="my-5">
+  <h1 className="font-bold mb-4 text-2xl">2. Limitations</h1> 
+<p className="mb-4">Pharetra nulla ullamcorper sit lectus. Fermentum mauris pellentesque nec nibh sed et, vel diam, massa. Placerat quis vel fames interdum urna lobortis sagittis sed pretium. Aliquam eget posuere sit enim elementum nulla vulputate magna. Morbi sed arcu proin quis tortor non risus</p>
+<p>Elementum lectus a porta commodo suspendisse arcu, aliquam lectus faucibus. Nisl malesuada tortor, ligula aliquet felis vitae enim. Mi augue aliquet mauris non elementum tincidunt eget facilisi. Pellentesque massa ipsum tempus vel aliquam massa eu pulvinar eget.</p>      
+</div>
+<div className="my-5">
+  <h1 className="font-bold mb-4 text-2xl">3. Revisions and Errata</h1> 
+<p className="mb-4">Pharetra nulla ullamcorper sit lectus. Fermentum mauris pellentesque nec nibh sed et, vel diam, massa. Placerat quis vel fames interdum urna lobortis sagittis sed pretium. Aliquam eget posuere sit enim elementum nulla vulputate magna. Morbi sed arcu proin quis tortor non risus</p>
+<p>Elementum lectus a porta commodo suspendisse arcu, aliquam lectus faucibus. Nisl malesuada tortor, ligula aliquet felis vitae enim. Mi augue aliquet mauris non elementum tincidunt eget facilisi. Pellentesque massa ipsum tempus vel aliquam massa eu pulvinar eget.</p>      
+</div>
+<div className="my-5">
+  <h1 className="font-bold mb-4 text-2xl">4. Site Terms of Use Modifications</h1> 
+<p className="mb-4">Pharetra nulla ullamcorper sit lectus. Fermentum mauris pellentesque nec nibh sed et, vel diam, massa. Placerat quis vel fames interdum urna lobortis sagittis sed pretium. Aliquam eget posuere sit enim elementum nulla vulputate magna. Morbi sed arcu proin quis tortor non risus</p>
+<p>Elementum lectus a porta commodo suspendisse arcu, aliquam lectus faucibus. Nisl malesuada tortor, ligula aliquet felis vitae enim. Mi augue aliquet mauris non elementum tincidunt eget facilisi. Pellentesque massa ipsum tempus vel aliquam massa eu pulvinar eget.</p>      
+</div>
+   
+
+
+</div>
+   
+  </div>
+</section>)
+};
+
+export default TremsPages;
--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
+import About from "../components/Pages/About/About";
 import AccountAgency from "../components/Pages/Account/AccountAgency";
 import AccountClient from "../components/Pages/Account/AccountClient";
 import Candidates from "../components/Pages/Candidates/Candidates";
+import Articles from "../components/Pages/Home/Articles/Articles";
+import SingelArticles from "../components/Pages/Home/Articles/SingelArticles";
 import Home from "../components/Pages/Home/Home";
 import Login from "../components/Pages/Login&Register/Login/Login";
 import ResetPass from "../components/Pages/Login&Register/Login/ResetPass";
 import Register from "../components/Pages/Login&Register/Register/Register";
 import NotFound from "../components/Pages/NotFound/NotFound";
+import TremsPages from "../components/Pages/TermsPages/TremsPages";
 import Main from "../layout/Main";
 
 export const router = createBrowserRouter([
@@ -50,5 +54,20 @@ export const router = createBrowserRouter([
     path: "/accountAgency",
     element: <AccountAgency />,
   },
+  {
+    path:'/about',
+    element:<About></About>
+  },
+  {
+    path:'/singelArticles',
+    element:<SingelArticles></SingelArticles>
+  },
+  {
+    path:"/trems",
+    element:<TremsPages></TremsPages>
+  },{
+    path:"/blogs",
+    element:<Articles></Articles>
+  }
 
 ]);
