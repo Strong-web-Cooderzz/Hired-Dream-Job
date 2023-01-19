a43b1831a52e19d4356d2c4b082a675c2a5b64a1
unknown <mojahidislammishat@gmail.com>
2023-01-19 18:45:57 +0000

    about single

---
+++ src/components/Pages/Home/Articles/SingelArticles.jsx
@@ -0,0 +1,169 @@
+import React from 'react';
+
+const SingelArticles = () => {
+
+  return (
+    <section>
+    <div>
+      <h2 className="text-center font-bold text-2xl mt-5">Attract Sales And Profits toward the sunshine - and shadows <br/> will fall behind you.</h2>
+      <div className="flex justify-center my-8">
+       <img className=" h-8 w-8 rounded-full ring-2 ring-white" src="https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg" alt=""/>
+       <h3 className="mx-3">Alison Dawn</h3>
+       <li className="mx-3">August 31, 2021</li>
+       <li>12 Comment</li>
+      </div>
+      <div className="my-8">
+        <img className="" src="https://i.pinimg.com/originals/a2/83/d7/a283d71bd2a2a4e9080dad4b79091d61.jpg" alt=""/>
+      </div>
+      <div className="my-8 lg:w-1/2 md:w-full mx-auto">
+        <h3 className="font-bold text-xl my-5">Course Description</h3>
+        <p className="">Aliquam hendrerit sollicitudin purus, quis rutrum mi accumsan nec. Quisque bibendum orci ac nibh facilisis, at malesuada orci  congue. Nullam tempus sollicitudin cursus. Ut et adipiscing erat. Curabitur this is a text link libero tempus congue.</p>
+    <p className="my-6">Duis mattis laoreet neque, et ornare neque sollicitudin at. Proin sagittis dolor sed mi elementum pretium. Donec et justo ante. Vivamus egestas sodales est, eu rhoncus urna semper eu. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Integer tristique elit lobortis purus bibendum, quis dictum metus mattis. Phasellus posuere felis sed eros porttitor mattis. Curabitur massa magna, tempor in blandit id, porta in ligula. Aliquam laoreet nisl massa, at interdum mauris sollicitudin et.</p>
+     
+  <div className="bg-teal-300 my-8">
+    <div className="p-8">
+      <p>Aliquam hendrerit sollicitudin purus, quis rutrum mi accumsan nec. Quisque bibendum orci ac nibh facilisis, at malesuada orci congue.</p>
+      <p className="mt-5">Luis Pickford</p>
+    </div>
+  </div>
+  <div>
+  {/* <!-- learn --> */}
+  <h2 className="my-6 text-lg font-semibold text-gray-900 dark:text-white">What you'll learn</h2>
+  <div className="grid lg:grid-cols-2  lg:grid-cols-1 gap-4">
+    <ul className="max-w-md space-y-1 ">
+      <li className="flex items-center">
+          <svg className="w-4 h-4 mr-3 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+          Become a UI/UX designer.
+      </li>
+      <li className="flex items-center">
+          <svg className="w-4 h-4 mr-3  flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+          You will be able to start earning money Figma skills.
+      </li>
+      <li className="flex items-center">
+        <svg className="w-4 h-4 mr-3  flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+        You will create your own UI Kit.
+      </li>
+      <li className="flex items-center">
+        <svg className="w-4 h-4 mr-3  flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+        Become a UI/UX designer.
+    </li>
+    <li className="flex items-center">
+        <svg className="w-4 h-4 mr-3  flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+        You will be able to start earning money Figma skills.
+    </li>
+   
+  </ul>
+  <ul className="max-w-md space-y-1 ">
+    <li className="flex items-center">
+        <svg className="w-4 h-4 mr-3 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+        Become a UI/UX designer.
+    </li>
+    <li className="flex items-center">
+        <svg className="w-4 h-4 mr-3  flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+        You will be able to start earning money Figma skills.
+    </li>
+    <li className="flex items-center">
+      <svg className="w-4 h-4 mr-3  flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+      You will create your own UI Kit.
+    </li>
+    <li className="flex items-center">
+      <svg className="w-4 h-4 mr-3  flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+      Become a UI/UX designer.
+  </li>
+  <li className="flex items-center">
+      <svg className="w-4 h-4 mr-3  flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+      You will be able to start earning money Figma skills.
+  </li>
+  
+  </ul>
+  </div>
+  
+  </div>
+  
+  <img className="w-full my-8" src="https://media.istockphoto.com/id/1335941248/photo/shot-of-a-handsome-young-man-standing-against-a-grey-background.jpg?b=1&s=170667a&w=0&k=20&c=Dl9uxPY_Xn159JiazEj0bknMkLxFdY7f4tK1GtOGmis=" alt=""/>
+  {/* <!-- Requirements --> */}
+  <div className="my-8">
+    <p className="my-5 font-bold">Requirements</p>
+    <li className="my-5">
+      We do not require any previous experience or pre-defined skills to take this course. A great orientation would be enough to master UI/UX design.
+    </li>
+    <li className="my-5">
+      A computer with a good internet connection.
+    </li>
+    <li className="my-5">
+      Adobe Photoshop OPTIONAL
+    </li>
+  </div>
+  <hr/>
+  {/* <!-- Share this post --> */}
+  <div className="my-8">
+    <div className="flex justify-between items-center"> 
+      <h3 className="font-bold">Share this post
+      </h3>
+      <button type="button" className=" px-4 py-2 font-bold text-white bg-blue-800 rounded focus:outline-none hover:bg-blue-900">Facebook</button> 
+      <button type="button" className=" px-4 py-2 font-bold text-white bg-red-600 rounded focus:outline-none hover:bg-red-800">Google</button>
+      <button type="button" className=" px-4 py-2 font-bold text-white bg-blue-500 rounded focus:outline-none hover:bg-blue-900" >Twitter</button>
+      <div className="my-6">
+        <button type="button" className=" mx-1 p-2 font-bold bg-gray-400 rounded focus:outline-none hover:bg-blue-900" >Design</button> 
+        <button type="button" className=" mx-1 p-2 font-bold bg-gray-400 rounded focus:outline-none hover:bg-blue-900">Digital</button> 
+         </div>
+       
+  
+    </div>
+    <hr className="mt-8"/>
+  </div>
+  {/* <!-- Comment --> */}
+  <div>
+  <h1 className="font-bold">Comment</h1>
+  </div>
+  <div className="flex my-8">
+    <img className="h-32 w-32 rounded-full ring-white" src="https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg" alt=""/>
+  <div className="mx-4">  
+    <h2 className="font-bold">Oscar Cafeo</h2>
+    <h3>Beautiful courses</h3>
+    <p className="mt-6">Far much that one rank beheld bluebird after outside ignobly allegedly more when oh arrogantly vehement tantaneously eel valiantly petted this along across highhandedly much.</p></div>
+   </div>
+   <div className="flex my-8">
+    <img className="h-32 w-32 rounded-full ring-white" src="https://media.sproutsocial.com/uploads/2022/06/profile-picture.jpeg" alt=""/>
+  <div className="mx-4">  
+    <h2 className="font-bold">Oscar Cafeo</h2>
+    <h3>Beautiful courses</h3>
+    <p className="mt-6">Far much that one rank beheld bluebird after outside ignobly allegedly more when oh arrogantly vehement tantaneously eel valiantly petted this along across highhandedly much.</p></div>
+   </div>
+   {/* <!-- Contact --> */}
+   <div>
+    <section className="bg-white dark:bg-gray-900 ">
+      <div className="py-8 lg:py-16 px-4 border-gray-600">
+          <h2 className="my-6 text-4xl tracking-tight font-bold text-gray-900 dark:text-white">Leave your thought here</h2>
+          <form action="#" className="space-y-8">
+             <div className="grid lg:grid-cols-2 md:grid-cols-1 gap-4">
+              <div>
+                <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your Name</label>
+                <input type="email" id="email" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Your Name" required/>
+            </div>
+            <div>
+              <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+              <input type="email" id="email" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Your Email" required/>
+          </div>
+             </div>
+              <div>
+                  <label for="subject" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+                  <input type="text" id="subject" className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" required/>
+              </div>
+              <div className="sm:col-span-2">
+                  <label for="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+                  <textarea id="message" rows="6" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+              </div>
+              <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-blue-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+          </form>
+      </div>
+    </section>
+   </div>
+  </div>
+     
+    </div>
+  </section>
+  );
+};
+
+export default SingelArticles;
