0906c90e2c3f70044f7bc9057fedac7ad5ae36d1
Abid Hasan <ab1dhasan@outlook.com>
2023-01-22 10:19:48 +0600

    added partial responsiveness to invoice
    this page is not that important

--- src/components/Pages/Invoice/Invoice.jsx
+++ src/components/Pages/Invoice/Invoice.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 export default function Invoice() {
 	return (
-		<main className="bg-blue-50 h-full py-20">
-			<section className="w-8/12 mx-auto">
+		<main className="bg-blue-50 h-full py-10 md:py-20">
+			<section className="w-11/12 md:w-8/12 mx-auto">
 				{/* buttons */}
 				<div className="flex gap-4 text-sm justify-end">
 					<Link className="py-3 bg-blue-100 px-8 rounded-md text-blue-700" to="/">Back Home</Link>
@@ -11,29 +11,32 @@ export default function Invoice() {
 				</div>
 
 				{/* invoice */}
-				<div className="mt-20 bg-white py-20 px-16 rounded-md">
-					<div className="flex">
-						<div className="w-7/12">
-							<span className="text-2xl font-bold">Logo</span>
-							{/* invoice date */}
-							<div className="flex flex-col mt-6">
-								<span className="text-sm text-gray-700">Invoice date:</span>
-								<span className="text-[0.85rem]">03/12/2023</span>
-							</div>
-							<div className="flex flex-col mt-16">
-								<span className="font-medium">Supplier</span>
-								<span className="text-[0.80rem] text-gray-800 mt-2">Hired Dream Job</span>
-								<span className="mt-2 text-xs text-gray-600">300, Whatever street, Dhaka</span>
-							</div>
-						</div>
-						<div className="w-5/12">
-							<div className="flex gap-20">
+				<div className="mt-10 md:mt-20 bg-white py-20 px-16 rounded-md">
+					{/* logo and invoice */}
+					<div className="flex-col">
+						<div className="flex flex-col md:flex-row gap-6 md:gap-0">
+							<span className="text-2xl font-bold md:w-7/12">Logo</span>
+							<div className="flex md:gap-20 flex-col md:flex-row">
 								<span className="text-2xl font-medium">Invoice #</span>
 								<span className="text-lg font-medium">123123123</span>
+							</div>
+						</div>
+						<div className="flex">
+							{/* invoice date */}
+							<div className="flex flex-col mt-6 md:w-7/12">
+								<span className="text-sm text-gray-700">Invoice date:</span>
+								<span className="text-[0.85rem]">03/12/2023</span>
 							</div>
 							<div className="flex flex-col mt-6">
 								<span className="text-sm text-gray-700">Due date:</span>
 								<span className="text-[0.85rem]">03/12/2023</span>
+							</div>
+						</div>
+						<div className="flex">
+							<div className="flex flex-col mt-16 md:w-7/12">
+								<span className="font-medium">Supplier</span>
+								<span className="text-[0.80rem] text-gray-800 mt-2">Hired Dream Job</span>
+								<span className="mt-2 text-xs text-gray-600">300, Whatever street, Dhaka</span>
 							</div>
 							<div className="flex flex-col mt-16">
 								<span className="font-medium">Supplier</span>
