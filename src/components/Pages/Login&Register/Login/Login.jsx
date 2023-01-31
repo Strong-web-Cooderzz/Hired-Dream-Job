9e6a6597554ffe13f27a6813c72346156d150a0c
Abid Hasan <ab1dhasan@outlook.com>
2023-01-31 12:58:54 +0600

    removed remember me from login
    it is useless since we are not using session login

--- src/components/Pages/Login&Register/Login/Login.jsx
+++ src/components/Pages/Login&Register/Login/Login.jsx
@@ -126,7 +126,7 @@ export default function Login() {
                 {errors.email && <span>{errors.email?.message}</span>}
               </div>
 
-              <div className="mb-6">
+              <div className="mb-2">
                 <input
                   type="password"
                   className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
@@ -140,22 +140,9 @@ export default function Login() {
               </div>
 
               <div className="flex justify-between items-center mb-6">
-                <div className="form-group form-check">
-                  <input
-                    type="checkbox"
-                    className="h-4 w-4 border border-gray-300 rounded-sm bg-white focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain float-left mr-2 cursor-pointer"
-                    id="exampleCheck2"
-                  />
-                  <label
-                    className="form-check-label inline-block text-gray-800"
-                    htmlFor="exampleCheck2"
-                  >
-                    Remember me
-                  </label>
-                </div>
                 <Link
                   to="/resetPass"
-                  className="text-gray-800 hover:text-blue-500"
+                  className="text-gray-800 hover:text-blue-500 text-sm"
                 >
                   Forgot password?
                 </Link>
@@ -176,6 +163,7 @@ export default function Login() {
                   >
                     Register
                   </Link>
+									{" "}here.
                 </p>
               </div>
             </form>
