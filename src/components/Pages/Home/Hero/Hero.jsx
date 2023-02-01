468662b0fc01a73c25ad45faed114958ef85b9da
Abid Hasan <ab1dhasan@outlook.com>
2023-02-01 16:12:55 +0600

    backend server link changed

--- src/components/Pages/Home/Hero/Hero.jsx
+++ src/components/Pages/Home/Hero/Hero.jsx
@@ -90,7 +90,7 @@ export const formAction = async ({ request }) => {
 		location: data.get('job-location')
 	}
 
-	const res = await fetch(`http://localhost:5000/find-jobs?search=${form.title}&location=${form.location}&sort=new&type=&time=0&per-page=10&page=1&experience=0`);
+	const res = await fetch(`https://hired-dream-job-server.vercel.app/find-jobs?search=${form.title}&location=${form.location}&sort=new&type=&time=0&per-page=10&page=1&experience=0`);
 	const fetchedData = await res.json();
 	// console.log(fetchedData);
 	return { form, fetchedData };
