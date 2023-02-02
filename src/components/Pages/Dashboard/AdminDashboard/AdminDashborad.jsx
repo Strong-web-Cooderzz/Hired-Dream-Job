a13233a6f7035f126c5ce11940c91c9199f35d3a
Abid Hasan <ab1dhasan@outlook.com>
2023-02-02 14:05:14 +0600

    added user type to admin dashboard

--- src/components/Pages/Dashboard/AdminDashboard/AdminDashborad.jsx
+++ src/components/Pages/Dashboard/AdminDashboard/AdminDashborad.jsx
@@ -1,12 +1,34 @@
+import { useEffect, useState } from "react";
 import Navbar from "../../../shared/Navbar/Navbar";
 
 export default function AdminDashboard() {
+	const [candidates, setCandidates] = useState([]);
+	const [employers, setEmployers] = useState([]);
+	const [userType, setUserType] = useState('candidates');
+
+	useEffect(() => {
+		fetch(`http://localhost:5000/api/v1/get/users`)
+			.then(res => res.json())
+			.then(data => {
+				console.log(data)
+				const newCandidate = data.filter(i => i.type === 'Candidate');
+				setCandidates(newCandidate);
+				const newEmployers = data.filter(i => i.type === 'Agency');
+				setEmployers(newEmployers);
+			});
+	}, []);
+	console.log(candidates)
+	console.log(employers)
+
 	return (
 		<>
 			<Navbar />
-			<div>
-				hello
-			</div>
+			<section>
+				<div className="flex flex-col lg:w-3/12 [&>span]:py-2 [&>span]:cursor-pointer [&>span]:rounded-md [&>span]:px-6 [&>span:hover]:bg-gray-300">
+					<span onClick={() => setUserType('candidates')} className={`${userType === 'candidates' && 'bg-gray-200'}`}>Candidates</span>
+					<span onClick={() => setUserType('employers')} className={`${userType === 'employers' && 'bg-gray-200'}`}>Employers</span>
+				</div>
+			</section>
 		</>
 	)
 }
