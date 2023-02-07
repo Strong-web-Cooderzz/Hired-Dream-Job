d3a84a00c7a2dc7ae5c68311f5c1f8818939f2e7
Arman Khan <sparmankhan@gmail.com>
2023-02-07 23:01:17 +0600

    manage blog post loading added

--- src/components/Pages/Dashboard/CandidatesDashboard/CandidateManageBlog/CandidateManageBlog.jsx
+++ src/components/Pages/Dashboard/CandidatesDashboard/CandidateManageBlog/CandidateManageBlog.jsx
@@ -6,6 +6,7 @@ import SingleBlog from './SingleBlog';
 
 const CandidateManageBlog = () => {
     const {user,dbUser} = useContext(AuthContext)
+    const [loading,setLoading] = useState(true)
     const [deleted,setDeleted] = useState(false)
     const [blogs,setBlogs] = useState([])
     useEffect(() => {
@@ -14,6 +15,7 @@ const CandidateManageBlog = () => {
         .then(data=>{
             setBlogs(data)
             console.log(data);
+            setLoading(false)
         })
     }, [user?.email,blogs,dbUser,deleted]);
     return (
@@ -22,11 +24,67 @@ const CandidateManageBlog = () => {
             <div>
                 <h3 className='text-xl'>Manage Blogs</h3>
             </div>
-            <div className=''>
+                 {
+                    loading ?
+                    <div>
+				 <div className="py-4 rounded shadow-md w-full sm:w-full animate-pulse bg-gray-50">
+                <div className="flex p-4 space-x-4 sm:px-8">
+                  <div className="flex-shrink-0 w-16 h-16 rounded-xl bg-gray-300"></div>
+                  <div className=" flex w-full gap-5">
+                    <div className="flex-1 py-2 space-y-4">
+                      <div className="w-64 h-3 rounded bg-gray-300"></div>
+                      <div className="w-8/12 h-3 rounded bg-gray-300"></div>
+                      <div className="flex gap-2 ">
+                        <div className="bg-gray-300 h-5 w-12 rounded-full"></div>
+                        <div className="bg-gray-300 h-5 w-12 rounded-full"></div>
+                        <div className="bg-gray-300 h-5 w-12 rounded-full"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+			  <div className="py-4 rounded shadow-md w-full sm:w-full animate-pulse bg-gray-50">
+                <div className="flex p-4 space-x-4 sm:px-8">
+                  <div className="flex-shrink-0 w-16 h-16 rounded-xl bg-gray-300"></div>
+                  <div className=" flex w-full gap-5">
+                    <div className="flex-1 py-2 space-y-4">
+                      <div className="w-64 h-3 rounded bg-gray-300"></div>
+                      <div className="w-8/12 h-3 rounded bg-gray-300"></div>
+                      <div className="flex gap-2 ">
+                        <div className="bg-gray-300 h-5 w-12 rounded-full"></div>
+                        <div className="bg-gray-300 h-5 w-12 rounded-full"></div>
+                        <div className="bg-gray-300 h-5 w-12 rounded-full"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+			  <div className="py-4 rounded shadow-md w-full sm:w-full animate-pulse bg-gray-50">
+                <div className="flex p-4 space-x-4 sm:px-8">
+                  <div className="flex-shrink-0 w-16 h-16 rounded-xl bg-gray-300"></div>
+                  <div className=" flex w-full gap-5">
+                    <div className="flex-1 py-2 space-y-4">
+                      <div className="w-64 h-3 rounded bg-gray-300"></div>
+                      <div className="w-8/12 h-3 rounded bg-gray-300"></div>
+                      <div className="flex gap-2 ">
+                        <div className="bg-gray-300 h-5 w-12 rounded-full"></div>
+                        <div className="bg-gray-300 h-5 w-12 rounded-full"></div>
+                        <div className="bg-gray-300 h-5 w-12 rounded-full"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+			 </div>
+                    : 
+                    <div className=''>
                 {
-                    blogs.map(blog=><SingleBlog setDeleted={setDeleted} deleted={deleted} key={blog._id} blog={blog} />)
+                    blogs.map(blog=><SingleBlog  setDeleted={setDeleted} deleted={deleted} key={blog._id} blog={blog} />)
                 }
             </div>
+                 }
+            
+           
         </div>
     );
 };
