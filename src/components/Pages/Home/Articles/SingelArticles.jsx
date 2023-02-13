c3527cc9eeba94a15f1706c2e1a5ecb991d7d575
Abid Hasan <ab1dhasan@outlook.com>
2023-02-13 12:34:08 +0600

    added spinner to single article

--- src/components/Pages/Home/Articles/SingelArticles.jsx
+++ src/components/Pages/Home/Articles/SingelArticles.jsx
@@ -14,6 +14,7 @@ import { toast } from 'react-hot-toast';
 
 const SingelArticles = () => {
 	const [post, setPost] = useState({});
+	const [dataLoading, setDataLoading] = useState(true);
 	const [isEmpty, setIsEmpty] = useState(true);
 	// delete confirmation modal
 	const [hidden, setHidden] = useState(true);
@@ -30,8 +31,8 @@ const SingelArticles = () => {
 	useEffect(() => {
 		async function fetchPost() {
 			const response = await fetchData.get(`/blogPost/${postId}`)
-			console.log(response.data[0])
 			setPost(response.data[0])
+			setDataLoading(false)
 		}
 
 		fetchPost();
@@ -79,85 +80,94 @@ const SingelArticles = () => {
 
 	return (
 		<section>
-			<div>
-				<div className='w-11/12 mx-auto md:w-full px-8 lg:w-8/12'>
-					<div className="flex items-center mt-12 gap-2 text-sm">
-						<img className="h-12 w-12 rounded-full ring-2 ring-white object-cover" src={post.author?.photo} alt="" />
-						<div>
-							<h3 className="text-lg">{post.author?.fullName}</h3>
-							<span className="mr-2 text-gray-600">{showDate(post.date)}</span>
-							<span className='text-sm mr-2 text-gray-600'>&#x2022;</span>
-							<span className='text-gray-600'>{post.comments?.length} Comment{post.comments?.length ? 's' : ''}</span>
-						</div>
-						{/* <!-- Share this post --> */}
-						<div className="ml-auto">
-							<div className="flex items-center">
-								<div className='flex gap-4 items-center text-gray-600'>
-									<a target={"_blank"} href={`https://facebook.com/sharer.php?u=${shareURL}`} className="text-lg"><FaFacebookF /></a>
-									<a target={"_blank"} href={`https://twitter.com/share?u=${shareURL}`} className="text-lg"><FaTwitter /></a>
-									<button type="button" className="text-lg"><FaLinkedinIn /></button>
+			{/* spinner */}
+			{
+				dataLoading && <>
+					<div className='w-24 h-24 bg-transparent rounded-full mx-auto border-[5px] border-blue-300 border-x-gray-200 border-b-gray-200 animate-spin my-16'>
+					</div>
+				</>
+			}
+			{
+				!dataLoading &&
+				<div>
+					<div className='w-11/12 mx-auto md:w-full px-8 lg:w-8/12'>
+						<div className="flex items-center mt-12 gap-2 text-sm">
+							<img className="h-12 w-12 rounded-full ring-2 ring-white object-cover" src={post.author?.photo} alt="" />
+							<div>
+								<h3 className="text-lg">{post.author?.fullName}</h3>
+								<span className="mr-2 text-gray-600">{showDate(post.date)}</span>
+								<span className='text-sm mr-2 text-gray-600'>&#x2022;</span>
+								<span className='text-gray-600'>{post.comments?.length} Comment{post.comments?.length ? 's' : ''}</span>
+							</div>
+							{/* <!-- Share this post --> */}
+							<div className="ml-auto">
+								<div className="flex items-center">
+									<div className='flex gap-4 items-center text-gray-600'>
+										<a target={"_blank"} href={`https://facebook.com/sharer.php?u=${shareURL}`} className="text-lg"><FaFacebookF /></a>
+										<a target={"_blank"} href={`https://twitter.com/share?u=${shareURL}`} className="text-lg"><FaTwitter /></a>
+										<button type="button" className="text-lg"><FaLinkedinIn /></button>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				</div>
-				<div className="my-8 mt-4 w-full flex justify-center">
-					<img className="" src={post.image} alt="" />
-				</div>
-				<div className="my-8 lg:w-8/12 md:w-full px-4 sm:px-8 lg:mx-auto">
-					<h2 className="font-semibold text-4xl mt-5">{post.title}</h2>
-					<div className='prose lg:prose-xl font-serif'>
-						<ReactMarkdown children={post.details} />
+					<div className="my-8 mt-4 w-full flex justify-center">
+						<img className="" src={post.image} alt="" />
 					</div>
-					<div className="my-6 mt-12 flex items-center justify-between">
-						{/* Tags */}
-						<div className='flex items-center gap-1'>
-							{
-								post?.tags?.map(tag => <button type="button" className="px-4 py-1 bg-blue-100 text-blue-700 text-sm rounded focus:outline-none hover:bg-blue-400 hover:text-white" >{tag.label}</button>)
-							}
+					<div className="my-8 lg:w-8/12 md:w-full px-4 sm:px-8 lg:mx-auto">
+						<h2 className="font-semibold text-4xl mt-5">{post.title}</h2>
+						<div className='prose lg:prose-xl font-serif'>
+							<ReactMarkdown children={post.details} />
 						</div>
-					</div>
-					{/* <!-- Comment --> */}
-					<div className='text-xl text-gray-600 flex gap-6 items-center'>
-						<span><FiThumbsUp /></span>
-						<span className='flex items-center gap-2'><FaRegComment /> <span>{post.comments?.length}</span></span>
-					</div>
-					{
-						userInfo.email ?
-							<form onSubmit={handlePostComment} className='mt-4'>
-								<div className='sm:flex w-full gap-1 items-center flex-col'>
-									<textarea onChange={e => {
-										e.target.value.length > 0 ? setIsEmpty(false) : setIsEmpty(true)
-									}} required name="comment" className='resize-none p-2 w-full h-16 border border-blue-400 focus-visible:outline-blue-600 rounded-xl'></textarea>
-									<button disabled={isEmpty} className={`mt-2 mr-auto ${isEmpty ? 'bg-gray-300 text-white cursor-not-allowed' : 'bg-blue-100 text-blue-700'} text-sm px-4 py-2 rounded-md`}>Add Comment</button>
-								</div>
-							</form>
-							:
-							<Link className='text-blue-600 font-bold' to={'/login'}>Please Login To Comment</Link>
-					}
-					{
-						post.comments?.map(comment => <div className="flex my-8">
-							<div className='w-1/12'>
-								<img className="h-14 w-14 rounded-full object-cover" src={comment.user.photo} alt={comment.user.fullName} />
-							</div>
-							<div className='w-11/12'>
-								<div className="bg-gray-200 rounded-lg px-4 py-2">
-									<h2 className="font-bold">{comment.user.fullName}</h2>
-									<p className="mt-3">{comment.comment}</p>
-								</div>
-								<div className='text-sm mt-1 flex gap-2 text-gray-700'>
-									<button>Report</button>
-									<button className={`${userInfo.email !== comment.user.email && 'hidden'} hover:text-red-500`} onClick={() => {
-										setHidden(false)
-										setCommentId(comment._id)
-									}}>Delete</button>
-									<ConfirmModal hidden={hidden} setHidden={setHidden} confirmFunction={handleDeleteComment} />
-								</div>
+						<div className="my-6 mt-12 flex items-center justify-between">
+							{/* Tags */}
+							<div className='flex items-center gap-1'>
+								{
+									post?.tags?.map(tag => <button type="button" className="px-4 py-1 bg-blue-100 text-blue-700 text-sm rounded focus:outline-none hover:bg-blue-400 hover:text-white" >{tag.label}</button>)
+								}
 							</div>
 						</div>
-						)}
-					{/* <!-- Contact --> */}
-					{/*<div>
+						{/* <!-- Comment --> */}
+						<div className='text-xl text-gray-600 flex gap-6 items-center'>
+							<span><FiThumbsUp /></span>
+							<span className='flex items-center gap-2'><FaRegComment /> <span>{post.comments?.length}</span></span>
+						</div>
+						{
+							userInfo.email ?
+								<form onSubmit={handlePostComment} className='mt-4'>
+									<div className='sm:flex w-full gap-1 items-center flex-col'>
+										<textarea onChange={e => {
+											e.target.value.length > 0 ? setIsEmpty(false) : setIsEmpty(true)
+										}} required name="comment" className='resize-none p-2 w-full h-16 border border-blue-400 focus-visible:outline-blue-600 rounded-xl'></textarea>
+										<button disabled={isEmpty} className={`mt-2 mr-auto ${isEmpty ? 'bg-gray-300 text-white cursor-not-allowed' : 'bg-blue-100 text-blue-700'} text-sm px-4 py-2 rounded-md`}>Add Comment</button>
+									</div>
+								</form>
+								:
+								<Link className='text-blue-600 font-bold' to={'/login'}>Please Login To Comment</Link>
+						}
+						{
+							post.comments?.map(comment => <div className="flex my-8">
+								<div className='w-1/12'>
+									<img className="h-14 w-14 rounded-full object-cover" src={comment.user.photo} alt={comment.user.fullName} />
+								</div>
+								<div className='w-11/12'>
+									<div className="bg-gray-200 rounded-lg px-4 py-2">
+										<h2 className="font-bold">{comment.user.fullName}</h2>
+										<p className="mt-3">{comment.comment}</p>
+									</div>
+									<div className='text-sm mt-1 flex gap-2 text-gray-700'>
+										<button>Report</button>
+										<button className={`${userInfo.email !== comment.user.email && 'hidden'} hover:text-red-500`} onClick={() => {
+											setHidden(false)
+											setCommentId(comment._id)
+										}}>Delete</button>
+										<ConfirmModal hidden={hidden} setHidden={setHidden} confirmFunction={handleDeleteComment} />
+									</div>
+								</div>
+							</div>
+							)}
+						{/* <!-- Contact --> */}
+						{/*<div>
 						<section className="bg-white dark:bg-gray-900 ">
 							<div className="py-8 lg:py-16 px-4 border-gray-600">
 								<h2 className="my-6 text-4xl tracking-tight font-bold text-gray-900 dark:text-white">Leave your thought here</h2>
@@ -185,8 +195,9 @@ const SingelArticles = () => {
 							</div>
 						</section>
 					</div>*/}
+					</div>
 				</div>
-			</div>
+			}
 		</section>
 	);
 };
