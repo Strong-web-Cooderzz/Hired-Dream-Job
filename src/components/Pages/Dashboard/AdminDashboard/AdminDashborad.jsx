5ed199b602731147940516afbe3590f2c900c4c0
Abid Hasan <ab1dhasan@outlook.com>
2023-02-15 15:57:24 +0600

    formatted admin dashboard

--- src/components/Pages/Dashboard/AdminDashboard/AdminDashborad.jsx
+++ src/components/Pages/Dashboard/AdminDashboard/AdminDashborad.jsx
@@ -1,19 +1,16 @@
 import { useEffect, useState } from "react";
 import Navbar from "../../../shared/Navbar/Navbar";
 import { BsFillTrashFill, BsGrid1X2, BsTrash2 } from "react-icons/bs"
-import {RxCrossCircled } from "react-icons/rx";
-import {IoWarningOutline } from "react-icons/io5";
-import {MdOutlineWorkOutline } from "react-icons/md";
-import {FiSettings, FiUsers } from "react-icons/fi";
-import {HiOutlineUsers } from "react-icons/hi";
+import { RxCrossCircled } from "react-icons/rx";
+import { IoWarningOutline } from "react-icons/io5";
+import { MdOutlineWorkOutline } from "react-icons/md";
+import { FiSettings, FiUsers } from "react-icons/fi";
+import { HiOutlineUsers } from "react-icons/hi";
 import Users from "./Routes/Users";
 import { Link, Outlet } from "react-router-dom";
 import { BiImageAlt } from "react-icons/bi";
 
 export default function AdminDashboard() {
-	
-
-
 	return (
 		<>
 			<Navbar />
@@ -22,20 +19,16 @@ export default function AdminDashboard() {
 				<section className="lg:w-3/12 border-r">
 					<div className="hidden md:flex flex-col [&>a]:py-2 [&>a]:cursor-pointer [&>a]:rounded-md [&>a]:px-6 [&>a:hover]:bg-gray-300">
 						<Link to={''} className={`flex false items-center gap-2 `}><BsGrid1X2 /> Dashboard</Link>
-
 						<Link to={'users'} className={`flex false items-center gap-2 `}><FiUsers /> Users</Link>
-
 						<Link to={'jobs'} className={`flex  items-center gap-2 `}><MdOutlineWorkOutline /> Jobs</Link>
-
 						<Link to={'blogs'} className={`flex  items-center gap-2 `}><FiSettings />Blogs</Link>
-
 						<Link to={'jobs'} className={`flex  items-center gap-2 `}><BiImageAlt />Themes</Link>
 					</div>
-					
 				</section>
-						<div className="w-full">
-						<Outlet/>
-						</div>
+
+				<div className="w-full">
+					<Outlet />
+				</div>
 			</main>
 		</>
 	)
