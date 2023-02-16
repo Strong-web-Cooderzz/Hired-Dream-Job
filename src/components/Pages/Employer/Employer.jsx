8a22598475aac3064e59c48ddcbee97363bde2c1
Abid Hasan <ab1dhasan@outlook.com>
2023-02-16 21:29:44 +0600

    created employer page dynamic

--- src/components/Pages/Employer/Employer.jsx
+++ src/components/Pages/Employer/Employer.jsx
@@ -5,14 +5,17 @@ import { BsFillBagFill, BsBagFill, BsClock, BsFacebook, BsGithub } from "react-i
 import { BsTelephone, BsLinkedin, BsBookmark } from "react-icons/bs";
 import { HiOutlineMail } from "react-icons/hi";
 import { GiCash } from "react-icons/gi";
+import fetchData from "../../../api/fetchData";
+import moment from "moment";
 
 export default function Employer() {
 	const employerId = useParams().id;
 	const [employer, setEmployer] = useState([]);
 	useEffect(() => {
-		fetch(`https://hired-dream-job-server-sparmankhan.vercel.app/employ/${employerId}`)
-			.then(res => res.json())
-			.then(data => setEmployer(data));
+		fetchData.get(`/employ/${employerId}`)
+			.then(response => {
+				setEmployer(response.data[0])
+			})
 	}, []);
 
 	console.log(employer);
@@ -20,80 +23,60 @@ export default function Employer() {
 		<>
 			<main className="flex flex-col-reverse lg:flex-col mb-16">
 				{/* short details */}
-					<div className="bg-gradient-to-r from-sky-400 to-purple-400 hidden lg:flex items-center px-12 py-6">
-						<img src={employer?.employData?.photo || employer?.photo} className="w-24 h-24 bg-green-300 rounded-full" />
+				<div className="bg-gradient-to-r from-sky-400 to-purple-400 hidden lg:flex items-center px-12 py-6">
+					<img src={employer?.employData?.photo || employer?.photo} className="w-24 h-24 rounded-full object-cover" />
 
-						{/* name */}
-						<div className="ml-4 flex flex-col gap-3">
-							<span className="text-2xl text-white font-semibold">{employer?.fullName}</span>
-							<div className="flex items-center gap-4 text-sm text-white">
-								<span className="flex items-center gap-1"><FaMapMarkerAlt />{employer?.address?.city + ', ' + employer?.address?.country}</span>
-								<span className="flex items-center gap-1"><BsFillBagFill />{employer?.type}</span>
-								<span className="flex items-center gap-1"><BsTelephone />{employer?.phoneNumber || 'N/A'}</span>
-								<span className="flex items-center gap-1"><HiOutlineMail />{employer?.email}</span>
-							</div>
-							<div>
-								<span className="text-xs text-blue-700 bg-blue-200 px-6 py-1 mt-1 rounded-full">Open Jobs - {employer?.available}</span>
-							</div>
+					{/* name */}
+					<div className="ml-4 flex flex-col gap-3">
+						<span className="text-2xl text-white font-semibold">{employer?.fullName}</span>
+						<div className="flex items-center gap-4 text-sm text-white">
+							<span className="flex items-center gap-1"><FaMapMarkerAlt />{employer?.address?.city + ', ' + employer?.address?.country}</span>
+							<span className="flex items-center gap-1"><BsFillBagFill />{employer?.type}</span>
+							<span className="flex items-center gap-1"><BsTelephone />{employer?.phoneNumber || 'N/A'}</span>
+							<span className="flex items-center gap-1"><HiOutlineMail />{employer?.email}</span>
 						</div>
+						<div>
+							<span className="text-xs text-blue-700 bg-blue-200 px-6 py-1 mt-1 rounded-full">Open Jobs - {employer?.jobs.length}</span>
+						</div>
+					</div>
 
-						{/* button
+					{/* button
 				<div className="ml-auto">
 					<button className="bg-blue-600 py-3 px-12 rounded-lg text-white hover:bg-blue-800">Private Message</button>
 				</div>
 					*/}
-					</div>
+				</div>
 
 				{/* details */}
 				<secction className="px-6 md:px-12 flex flex-col lg:flex-row gap-8 md:gap-16 mt-16">
 					<div className="lg:w-9/12 flex flex-col">
-						<span className="text-lg font-bold">About Company</span>
-						<span className="text-sm mt-4 text-gray-600">{employer.employData?.Company_Bio}</span>
-						<span className="text-xl font-medium mt-6">3 other jobs available</span>
+						<span className="text-2xl font-bold">About Company</span>
+						<span className="text-md mt-2 text-gray-600">{employer.employData?.Company_Bio}</span>
+						<span className="text-xl font-medium mt-8">{employer.jobs.length} other jobs available</span>
 						{/* cards */}
-						<div className="flex flex-col gap-8 mt-4">
-							<div className="p-5 border rounded-md flex flex-col gap-4">
-								<span className="font-medium">Software Engineer</span>
-								<div className="flex text-xs text-gray-600 gap-4">
-									<span className="flex items-center gap-2"><BsBagFill /> <span>Segment</span></span>
-									<span className="flex items-center gap-2"><FaMapMarkerAlt /> <span>Segment</span></span>
-									<span className="md:flex items-center gap-2 hidden"><BsClock /> <span>11 hours ago</span></span>
-									<span className="md:flex items-center gap-2 hidden"><GiCash /> <span>$35k -$45k</span></span>
-								</div>
-								<div className="text-xs flex gap-4">
-									<span className="py-1 px-4 bg-blue-100 rounded-full text-blue-700">Full Time</span>
-									<span className="py-1 px-4 bg-green-100 rounded-full text-green-700">Private</span>
-									<span className="py-1 px-4 bg-yellow-100 rounded-full text-yellow-700">Urgent</span>
-								</div>
-							</div>
-							<div className="p-5 border rounded-md flex flex-col gap-4">
-								<span className="font-medium">Software Engineer</span>
-								<div className="flex text-xs text-gray-600 gap-4">
-									<span className="flex items-center gap-2"><BsBagFill /> <span>Segment</span></span>
-									<span className="flex items-center gap-2"><FaMapMarkerAlt /> <span>Segment</span></span>
-									<span className="md:flex items-center gap-2 hidden"><BsClock /> <span>11 hours ago</span></span>
-									<span className="md:flex items-center gap-2 hidden"><GiCash /> <span>$35k -$45k</span></span>
-								</div>
-								<div className="text-xs flex gap-4">
-									<span className="py-1 px-4 bg-blue-100 rounded-full text-blue-700">Full Time</span>
-									<span className="py-1 px-4 bg-green-100 rounded-full text-green-700">Private</span>
-									<span className="py-1 px-4 bg-yellow-100 rounded-full text-yellow-700">Urgent</span>
-								</div>
-							</div>
-							<div className="p-5 border rounded-md flex flex-col gap-4">
-								<span className="font-medium">Software Engineer</span>
-								<div className="flex text-xs text-gray-600 gap-4">
-									<span className="flex items-center gap-2"><BsBagFill /> <span>Segment</span></span>
-									<span className="flex items-center gap-2"><FaMapMarkerAlt /> <span>Segment</span></span>
-									<span className="md:flex items-center gap-2 hidden"><BsClock /> <span>11 hours ago</span></span>
-									<span className="md:flex items-center gap-2 hidden"><GiCash /> <span>$35k -$45k</span></span>
-								</div>
-								<div className="text-xs flex gap-4">
-									<span className="py-1 px-4 bg-blue-100 rounded-full text-blue-700">Full Time</span>
-									<span className="py-1 px-4 bg-green-100 rounded-full text-green-700">Private</span>
-									<span className="py-1 px-4 bg-yellow-100 rounded-full text-yellow-700">Urgent</span>
-								</div>
-							</div>
+						<div className="flex flex-col gap-4 mt-4">
+							{
+								employer.jobs.map(job => <>
+									<div className="p-5 border rounded-md flex flex-col gap-4">
+										<span className="font-medium">{job.title}</span>
+										<div className="flex text-xs text-gray-600 gap-4">
+											<span className="flex items-center gap-2"><BsBagFill /><span>{job.category}</span></span>
+											<span className="flex items-center gap-2"><FaMapMarkerAlt /> <span>{job.location}</span></span>
+											<span className="md:flex items-center gap-2 hidden"><BsClock /><span>{moment(job.postTime).fromNow()}</span></span>
+											<span className="md:flex items-center gap-2 hidden"><GiCash /> <span>${job.salaryMin} - ${job.salaryMax}</span></span>
+										</div>
+										<div className="text-xs flex gap-4">
+											<span className="py-1 px-4 bg-blue-100 rounded-full text-blue-700">{job.jobType}</span>
+											<span className="py-1 px-4 bg-green-100 rounded-full text-green-700">{job.companyType}</span>
+											{
+												job.urgent &&
+												<span className="py-1 px-4 bg-yellow-100 rounded-full text-yellow-700">Urgent</span>
+											}
+										</div>
+									</div>
+								</>
+								)
+							}
 						</div>
 					</div>
 
@@ -105,7 +88,7 @@ export default function Employer() {
 								<span className="text-lg text-blue-700 bg-blue-100 p-3 rounded-lg"><BsBookmark /></span>
 							</div>
 						</div>
-						<div className="flex flex-col bg-blue-50 p-6 gap-4">
+						<div className="flex flex-col bg-gray-50 p-6 gap-4">
 							<div className="hidden md:flex lg:hidden items-center gap-4">
 								{/* shows company image */}
 								<div className="w-16 h-16 bg-red-300 rounded-full">
@@ -115,21 +98,19 @@ export default function Employer() {
 									<span className="bg-blue-200 px-4 rounded-full py-1 text-xs">Open Jobs - {employer?.available}</span>
 								</div>
 							</div>
-							<span className="flex items-center justify-between text-md">Primary industry: <span className="text-gray-500 text-sm">Software</span></span>
-							<span className="flex items-center justify-between text-md">Company Size: <span className="text-gray-500 text-sm">{employer.employData?.team}</span></span>
-
-							<span className="flex items-center justify-between text-md">Founded in: <span className="text-gray-500 text-sm">{employer.employData?.build}</span></span>
-							<span className="flex items-center justify-between text-md">Phone: <span className="text-gray-500 text-sm">{employer.employData?.phoneNumber}</span></span>
+							<span className="flex items-center justify-between text-md">Company Size: <span className="text-gray-500 text-md">{employer.employData?.team}</span></span>
+							<span className="flex items-center justify-between text-md">Founded in: <span className="text-gray-500 text-md">{moment(employer.founded).fromNow()}</span></span>
+							<span className="flex items-center justify-between text-md">Phone: <span className="text-gray-500 text-md">{employer.employData?.phoneNumber}</span></span>
 							<span className="flex items-center justify-between text-md">Location: <span className="text-gray-500 text-md">{employer.address?.city + ', ' + employer.address?.country}</span></span>
 							<span className="flex items-center justify-between text-md">Social media:
-								<span className="text-gray-500 text-sm flex items-center gap-1">
-									<a href={employer.employData?.facebook} className="text-lg"><BsFacebook /></a>
-									<a href={employer.employData?.linkedin} className="text-lg"><BsLinkedin /></a>
+								<span className="text-gray-500 text-sm flex items-center gap-2">
+									<a href={employer.social?.facebook} className="text-lg"><BsFacebook /></a>
+									<a href={employer.social?.linkedin} className="text-lg"><BsLinkedin /></a>
 									<a href={employer.employData?.github} className="text-lg"><BsGithub /></a>
 								</span>
 							</span>
 
-							<a className="bg-blue-200 flex justify-center py-3 text-blue-700 rounded-md" href={employer.employData?.website}><button>{employer.website}</button></a>
+							<a className="bg-blue-200 flex justify-center py-3 text-blue-700 rounded-md" href={employer.website}>{employer.website || 'No link found'}</a>
 
 						</div>
 					</div>
