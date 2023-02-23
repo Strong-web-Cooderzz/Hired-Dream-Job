ddcf4e75e9970f398f797678267241b8d618d258
Abid Hasan <ab1dhasan@outlook.com>
2023-02-23 13:31:40 +0600

    modified routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,9 +4,11 @@ import AccountAgency from "../components/Pages/Account/AccountAgency";
 import AccountClient from "../components/Pages/Account/AccountClient";
 import Candidate from "../components/Pages/Candidates/Candidate";
 import Candidates from "../components/Pages/Candidates/Candidates";
+
 import DashboardAddPost from "../components/Pages/Dashboard/DashboardAddPost/DashboardAddPost";
 import MyAllPost from "../components/Pages/Dashboard/MyAllPost/MyAllPost";
 import EmployerProfile from "../components/Pages/Dashboard/MyProfle/EmployerProfile";
+
 import Articles from "../components/Pages/Home/Articles/Articles";
 import SingelArticles from "../components/Pages/Home/Articles/SingelArticles";
 import Contact from "../components/Pages/Contact/Contact";
@@ -31,12 +33,14 @@ import Profile from "../components/Pages/Dashboard/MyProfle/Profile";
 import ManageJobs from "../components/Pages/Dashboard/EmployeeDashboard/ManageJobs/ManageJobs";
 import EditJob from "../components/Pages/Dashboard/EmployeeDashboard/ManageJobs/Modal/EditJob";
 import Payment from "../components/Pages/Dashboard/EmployeeDashboard/ManageJobs/FeaturedJob/Payment/Payment";
+
 import AdminDashboard from "../components/Pages/Dashboard/AdminDashboard/AdminDashborad";
 import CandidateAddpost from "../components/Pages/Dashboard/CandidatesDashboard/CandidateAddpost/CandidateAddpost";
 import CandidateManageBlog from "../components/Pages/Dashboard/CandidatesDashboard/CandidateManageBlog/CandidateManageBlog";
 import EditBlog from "../components/Pages/Dashboard/CandidatesDashboard/CandidateManageBlog/EditBlog";
 import Users from "../components/Pages/Dashboard/AdminDashboard/Routes/Users";
 import Jobs from "../components/Pages/Dashboard/AdminDashboard/Routes/Jobs";
+
 import DashboardDetails from "../components/Pages/Dashboard/AdminDashboard/Routes/DashboardDetails";
 import ManageUsers from "../components/Pages/Dashboard/AdminDashboard/Routes/ManageUsers";
 import ManageAdminJob from "../components/Pages/Dashboard/EmployeeDashboard/ManageJobs/ManageJob";
@@ -45,6 +49,7 @@ import Blogs from "../components/Pages/Dashboard/AdminDashboard/Routes/Blogs";
 import AppliedCandidates from './../components/Pages/Dashboard/EmployeeDashboard/AppliedCandidates/AppliedCandidates';
 import EditEmployer from "../components/Pages/Dashboard/MyProfle/Modal/EditEmployer";
 import EditCandidate from "../components/Pages/Dashboard/MyProfle/Modal/EditCandidate";
+
 
 export const router = createBrowserRouter([
 	{
@@ -230,3 +235,4 @@ export const router = createBrowserRouter([
 		element: <NotFound />,
 	},
 ]);
+
