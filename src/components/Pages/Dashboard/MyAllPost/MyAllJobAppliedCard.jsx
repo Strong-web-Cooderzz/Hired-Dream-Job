b1c4da0ac79206d6aff485d95808922de21a0f7d
Abid Hasan <ab1dhasan@outlook.com>
2023-02-24 18:04:02 +0600

    modified all applied jobs

--- src/components/Pages/Dashboard/MyAllPost/MyAllJobAppliedCard.jsx
+++ src/components/Pages/Dashboard/MyAllPost/MyAllJobAppliedCard.jsx
@@ -1,35 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../AuthProvider/AuthProvider';
 
-const MyAllJobAppliedCard = ({appliedJobInfo}) => {
-    const {candidateName,applyDate,candidateMessage , candidateResume,company,companyLocation,companyType,jobTitle,jobType
-    } = appliedJobInfo ;
-    console.log(appliedJobInfo)
-    return (
-        <div>
-    <div className="flex justify-center ">
-  <div className="block rounded-lg shadow-lg bg-white max-w-sm text-center">
-    <div className="py-3 px-6 border-b border-gray-300">
-      Company  name:   <span className='font-semibold'> {company} </span>
-    </div>
-     <p> Company  type: {companyType} </p>
-    <div className="p-6 text-start">
-      <h5 className="text-gray-900 text-xl mb-2"> {jobTitle} </h5>
-      <h2 > Candidate name:  {candidateName} </h2>
-      <h2> Job type: {jobType} </h2>
-      <p className="text-gray-700 text-base mb-4">
-        {candidateMessage}
-      </p>
-      <p> Company location:  {companyLocation} </p>
-      <p> Applied date: {applyDate} </p>
-       <a className='text-blue-500 font-medium hover:text-blue-700 ' href={candidateResume} > My Resume </a> 
-      <button type="button" className=" inline-block float-right mb-3 px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0  transition duration-150 ease-in-out">  Delete </button>
-    </div>
-  
-  </div>
-</div>
-        </div>
-    );
+const MyAllJobAppliedCard = ({ appliedJobInfo }) => {
+	const {user} = useContext(AuthContext)
+	// const {candidateName,applyDate,candidateMessage , candidateResume,company,companyLocation,companyType,jobTitle,jobType
+	// } = appliedJobInfo ;
+	// console.log(appliedJobInfo)
+	return (
+		<div>
+			<div className="flex justify-center ">
+				<div className="block rounded-lg shadow-lg bg-white max-w-sm text-center">
+					<div className="py-3 px-6 border-b border-gray-300">
+						Company  name:   <span className='font-semibold'> {appliedJobInfo.company.fullName} </span>
+					</div>
+					<p> Company  type: {appliedJobInfo.companyType} </p>
+					<div className="p-6 text-start">
+						<h5 className="text-gray-900 text-xl mb-2"> {appliedJobInfo.jobTitle} </h5>
+						<h2 > Candidate name:  {user?.displayName} </h2>
+						<h2> Job type: {appliedJobInfo.jobType} </h2>
+						<p className="text-gray-700 text-base mb-4">
+							{appliedJobInfo.candidateMessage}
+						</p>
+						<p> Company location:  {appliedJobInfo?.companyLocation} </p>
+						<p> Applied date: {appliedJobInfo.applyDate} </p>
+						<a className='text-blue-500 font-medium hover:text-blue-700 ' href={appliedJobInfo.candidateResume} > My Resume </a>
+						<button type="button" className=" inline-block float-right mb-3 px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0  transition duration-150 ease-in-out">  Delete </button>
+					</div>
+
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MyAllJobAppliedCard;
--- src/components/Pages/Dashboard/MyAllPost/MyAllPost.jsx
+++ src/components/Pages/Dashboard/MyAllPost/MyAllPost.jsx
@@ -1,22 +1,29 @@
 import React, { useContext, useEffect, useState } from "react";
+import fetchData from "../../../../api/fetchData";
 import { AuthContext } from "../../../AuthProvider/AuthProvider";
 import MyAllJobAppliedCard from "./MyAllJobAppliedCard";
 
 const MyAllPost = () => {
-  const { user } = useContext(AuthContext);
-  const [appliedJobData, setAppliedJobData] = useState([]);
+	const { user } = useContext(AuthContext);
+	const [appliedJobData, setAppliedJobData] = useState([]);
 
-  const url = `https://hdj-server.vercel.app/job-applied-post/${user?.uid}`;
-  useEffect(() => {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        setAppliedJobData(data);
-      });
-  }, []);
+	// const url = `https://hdj-server.vercel.app/job-applied-post/${user?.uid}`;
+	useEffect(() => {
+		if (user.uid) {
+			fetchData.get(`/job-applied-post/${user?.uid}`)
+				.then(response => {
+					setAppliedJobData(response.data);
+					console.log(response.data)
+				})
+		}
+		// fetch(url)
+		//   .then((res) => res.json())
+		//   .then((data) => {
+		//   });
+	}, [user]);
 
-  return (
-    <div className="w-[90%] mx-12">
+	return (
+		<div className="w-[90%] mx-12">
 			<div className="flex justify-start">
 				{/* <h2 className="text-2xl">Add new Blog</h2> */}
 			</div>
@@ -24,20 +31,20 @@ const MyAllPost = () => {
 				<div className="w-full">
 					<h2 className="text-xl px-3 py-5">All Applied Jobs</h2>
 				</div>
-      {/* See all  applied job post   */}
-      <div className="grid lg:grid-cols-2 lg:gap-0 gap-y-5">
-        {appliedJobData.map((appliedJobInfo) => (
-          <MyAllJobAppliedCard
-            key={appliedJobInfo._id}
-            appliedJobInfo={appliedJobInfo}
-          >
-            {" "}
-          </MyAllJobAppliedCard>
-        ))}
-      </div>
-    </div>
-    </div>
-  );
+				{/* See all  applied job post   */}
+				<div className="grid lg:grid-cols-2 lg:gap-0 gap-y-5">
+					{appliedJobData.map((appliedJobInfo) => (
+						<MyAllJobAppliedCard
+							key={appliedJobInfo._id}
+							appliedJobInfo={appliedJobInfo}
+						>
+							{" "}
+						</MyAllJobAppliedCard>
+					))}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default MyAllPost;
