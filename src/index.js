4c187c6d8ea60b987a20502b2f4d3c3d62bae308
Abid Hasan <ab1dh4san@gmail.com>
2023-03-08 14:50:54 +0600

    modified index.js

--- src/index.js
+++ src/index.js
@@ -7,13 +7,14 @@ import 'tw-elements';
 import { Provider } from "react-redux";
 import { store } from "./app/store";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
 	<React.StrictMode>
 		<Provider store={store}>
 			<AuthProvider>
 				<App />
-				
 			</AuthProvider>
 		</Provider>
 	</React.StrictMode>
 );
+
