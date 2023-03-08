b89076ded1bc096ad216119976a90f6150547e43
Abid Hasan <ab1dh4san@gmail.com>
2023-03-08 14:42:27 +0600

    modified package.json

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./src/**/*.{js,jsx,ts,tsx}", 
+		'./node_modules/tw-elements/dist/js/**/*.js'
+	],
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('tw-elements/dist/plugin')
+	]
+};
