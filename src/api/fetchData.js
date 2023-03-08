42631862082cbfcc6239810e71f941172e8fbe56
Abid Hasan <ab1dh4san@gmail.com>
2023-03-08 20:42:48 +0600

    modified api link

--- src/api/fetchData.js
+++ src/api/fetchData.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default axios.create({
-	baseURL: 'https://hired-dream-job-server.vercel.app'
+	// baseURL: 'https://hired-dream-job-server.vercel.app'
 	// baseURL: "http://localhost:5000",
+	baseURL: "https://hdj-server.vercel.app"
 	// baseURL: "https://hdj-server.onrender.com",
 	// baseURL: 'https://hired-dream-job-server-sparmankhan.vercel.app'
 });
